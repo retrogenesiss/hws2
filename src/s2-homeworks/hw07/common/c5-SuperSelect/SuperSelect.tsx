0f7f0eed9b62fa7dcf9d0e6bdbf1518545c92e0c
RetroGenesis <arthurbenyash@gmail.com>
2023-03-19 12:35:07 +0300

    commit description

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,7 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         if (onChangeOption) {
-            onChangeOption(String(e.currentTarget.value))
+            onChangeOption(Number(e.currentTarget.value))
         }
     }
 
