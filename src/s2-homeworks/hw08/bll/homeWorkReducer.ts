4511877d234b4483df0312cb5561dafe9f72ba95
RetroGenesis <arthurbenyash@gmail.com>
2023-03-22 13:05:33 +0300

    commit description

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,16 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const payload = action.payload
+            return state.map(el => payload === 'up' ? {...el, _id: + 1}
+                : payload === 'down' ? {...el, _id: 0} : el)
         }
         case 'check': {
-
-            return state // need to fix
+            const payload = action.payload
+            return state.filter(el => el.age > payload)
         }
         default:
             return state
